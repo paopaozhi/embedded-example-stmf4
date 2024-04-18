306a8be7cde42a38084741b7f80970c861ce04b5
泡泡纸 <paopaozhi@hotmail.com>
2024-04-18 18:17:26 +0800

    feat:downloaddrelease pack

--- utility.py
+++ utility.py
@@ -1,13 +1,46 @@
 import os
-from git.repo import Repo
+from git.exc import GitCommandError
+from git import Repo
 import toml
-import json
+import requests
 from rich import print
-from rich import print_json
+from rich import progress
+from rich.progress import Progress
+from rich.progress import TextColumn,TimeElapsedColumn,TaskProgressColumn,SpinnerColumn,BarColumn
 
 cfg_file = toml.load("./depend.toml")
 
-print_json(json.dumps(cfg_file))
+depend_lib = cfg_file["depend"]
 
-download_path = os.path.join('build','lib','ulog')
-Repo.clone_from(cfg_file["depend"]["ulog"]["url"],to_path=download_path)
+def download_release(url,name,path):
+    """
+    下载release包到指定路径
+    """
+    ret = requests.get(url,stream=True)
+
+    with open(f'{name}.zip','wb') as fd,Progress(
+        TextColumn("[progress.description]{task.description}"),
+        SpinnerColumn(),
+        TimeElapsedColumn(),
+    ) as progress:
+        task1 = progress.add_task(f"[Download {name}..]")
+        for data in ret.iter_content(chunk_size=1024):
+            size = fd.write(data)
+            progress.update(task1)
+
+with Progress(
+    TextColumn("[progress.description]{task.description}"),
+    BarColumn(),
+    TaskProgressColumn(),
+    TimeElapsedColumn(),
+) as progress:
+    task1 = progress.add_task("Download lib...", total=len(depend_lib))
+
+    for lib_name in depend_lib:
+        lib_url = depend_lib[lib_name]["url"]
+        download_path = os.path.join('build','lib',lib_name)
+        try:
+            Repo.clone_from(lib_url,to_path=download_path)
+        except GitCommandError:
+            pass
+        progress.update(task1,advance=1)
